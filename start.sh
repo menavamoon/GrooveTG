093f69f4a52cc6e22d6eed596debc8c5c4c4e61c
ElenLiL <116087760+elenlilco@users.noreply.github.com>
2022-11-05 04:45:31 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/elenlilco/GrooveTG /GrooveTG
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/elenlilco/GrooveTG -b $BRANCH /GrooveTG
 fi
-cd /VCPlayerBot
+cd /GrooveTG
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
